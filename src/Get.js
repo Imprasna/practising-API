8bf5263fbe7753387dfdf5891ca558500ceabd19
Imprasna <prasanna@sibbc.org>
2021-11-11 16:56:19 +0530

    Alignment was done.

--- src/Get.js
+++ src/Get.js
@@ -18,7 +18,7 @@ import Humidity from "./component/Humidity";
 import WindDirection from "./component/WindDirection";
 import WindSpeed from "./component/WindSpeed";
 import Uv from "./component/Uv";
-import './component/style.css';
+import "./component/style.css";
 
 const Get = () => {
 	const [getPost, setGetPost] = useState([]);
@@ -38,7 +38,9 @@ const Get = () => {
 	const getData = async () => {
 		const city = values.cities;
 		try {
-			const res = await axios.get(`https://cors-anywhere.herokuapp.com/http://api.weatherapi.com/v1/current.json?key=b572a200fc624a6a85671436211310&q=${city}&aqi=yes`);
+			const res = await axios.get(
+				`https://cors-anywhere.herokuapp.com/http://api.weatherapi.com/v1/current.json?key=b572a200fc624a6a85671436211310&q=${city}&aqi=yes`
+			);
 			// console.log(res.data.current);
 			// console.log(res.data.location);
 			setGetPost(res.data);
@@ -63,103 +65,108 @@ const Get = () => {
 
 	return (
 		<>
-            <input
-                className="search-box"
+			<input
+				className="search-box"
 				type="text"
 				value={values.cities}
 				name="cities"
 				onChange={(e) => onChangeHandler(e)}
 			/>
-			<button className="button" onClick={getData}>Submit</button>
+			<button className="button" onClick={getData}>
+				Submit
+			</button>
 
-            <div className="whole">
-                
-                <div className="Country">
-                    <h1>Country</h1>
-                    <Country CName={getPost} />
-                </div>
+			<div className="whole">
+				<div className="Country">
+					<h1>Country</h1>
+					<Country CName={getPost} />
+				</div>
 
-                <div className="region">
-                <h1>Region</h1>
-                <Region CName={getPost} />
-                </div>
+				<div className="region">
+					<h1>Region</h1>
+					<Region CName={getPost} />
+				</div>
 
-                <div className="time">
-                <h1>Day / Time</h1>
-                <Time CName={getPost} />
-                </div>
+				<div className="time">
+					<h1>Day / Time</h1>
+					<Time CName={getPost} />
+				</div>
 
-                <div className="continent">
-                <h1>Continent</h1>
-                <Continent CName={getPost} />
-                </div>
+				<div className="continent">
+					<h1>Continent</h1>
+					<Continent CName={getPost} />
+				</div>
 
-                <div className="district">
-                <h1>District</h1>
-                <District CName={getPost} />
-                </div>
+				<div className="district">
+					<h1>District</h1>
+					<District CName={getPost} />
+				</div>
 
-                <div className="lat">
-                <h1>Latitude</h1>
-                    <Latitude CName={getPost} />
-                </div>
+				<div className="lat">
+					<h1>Latitude</h1>
+					<Latitude CName={getPost} />
+				</div>
 
-                <div className="lon">
-                <h1>Longitude</h1>
-                <Longitude CName={getPost} />
-                </div>
+				<div className="lon">
+					<h1>Longitude</h1>
+					<Longitude CName={getPost} />
+				</div>
 
-                <div className="cloud">
-                <h1>Cloud (oktas)</h1>
-                <Cloud CName={getPost} />
-                </div>
+				<div className="cloud">
+					<h1>Cloud (oktas)</h1>
+					<Cloud CName={getPost} />
+				</div>
 
-                <div className="upt">
-                <h1>Info Updated on</h1>
-                <UpdatedWeather CName={getPost} />
-                </div>
+				<div className="upt">
+					<h1>Info Updated on</h1>
+					<UpdatedWeather CName={getPost} />
+				</div>
 
-                <div className="cel">
-                <h1>Celcius</h1>
-                <Celcius CName={getPost} />
-                </div>
+				<div className="cel">
+					<h1>Celcius</h1>
+					<Celcius CName={getPost} />
+				</div>
 
-                <div className="far">
-                <h1>Farenheit</h1>
-                <Farenheit CName={getPost} />
-                </div>
+				<div className="far">
+					<h1>Farenheit</h1>
+					<Farenheit CName={getPost} />
+				</div>
 
-                <div className="cond">
-                <h1>Condition</h1>
-                <Condition CName={getPost} />
-                </div>
+				<div className="cond">
+					<h1>Condition</h1>
+					<Condition CName={getPost} />
+				</div>
 
-                <div className="icon">
-                <h1>Icon</h1>
-                <Icon CName={getPost} />
-                </div>
+				<div className="icon">
+					<h1>Icon</h1>
+					<Icon CName={getPost} />
+				</div>
 
-                <div className="hum">
-                <h1>Humidity (<sup>g.m-3</sup>)</h1>
-                <Humidity CName={getPost} />
-                </div>
+				<div className="hum">
+					<h1>
+						Humidity (<sup>g.m-3</sup>)
+					</h1>
+					<Humidity CName={getPost} />
+				</div>
 
-                <div className="wd">
-                <h1>Wind Direction</h1>
-                <WindDirection CName={getPost} />
-                </div>
+				<div className="wd">
+					<h1>Wind Direction</h1>
+					<WindDirection CName={getPost} />
+				</div>
 
-                <div className="ws">
-                <h1>Wind Speed (<sup>kph</sup>)</h1>
-                <WindSpeed CName={getPost} />
-                </div>
+				<div className="ws">
+					<h1>
+						Wind Speed (<sup>kph</sup>)
+					</h1>
+					<WindSpeed CName={getPost} />
+				</div>
 
-                <div className="uv">
-                <h1>UV Ray (<sup>mW/cm2</sup>)</h1>
-                    <Uv CName={getPost} />
-                </div>
-
-                
+				<div className="uv">
+					<h1>
+						UV Ray (<sup>mW/cm2</sup>)
+					</h1>
+					<Uv CName={getPost} />
+				</div>
 			</div>
 		</>
 	);
